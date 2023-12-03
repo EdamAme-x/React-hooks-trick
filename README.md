f8166e49016c22870848c893e86a82d2684b3539
Ame_x <121654029+EdamAme-x@users.noreply.github.com>
2023-12-03 17:53:59 +0900

    add

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # React-trick
 React trick
+
+[BY](https://www.w3schools.com/react) 
---
+++ useCallback.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { memo } from "react";
+
+const Todos = memo(({ todos, addTodo }) => {
+  console.log("child render");
+  return (
+    <>
+      <h2>My Todos</h2>
+      {todos.map((todo, index) => {
+        return <p key={index}>{todo}</p>;
+      })}
+      <button onClick={addTodo}>Add Todo</button>
+    </>
+  );
+});
+
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+  const addTodo = () => {
+    setTodos((t) => [...t, "New Todo"]);
+  };
+
+  return (
+    <>
+      <Todos todos={todos} addTodo={addTodo} />
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+      </div>
+    </>
+  );
+};
